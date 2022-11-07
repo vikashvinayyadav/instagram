9ab8df88dbaddeb0d4c1a26b5f751134e67645c8
vikashvinayyadav <vikashvinayyadav@gmail.com>
2022-11-07 18:49:33 +0530

    'lib/assets/logo.png'

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,21 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram/screens/main_screen.dart';
-import '../utilis/colors.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main()async{
-  WidgetsFlutterBinding.ensureInitialized();
-   if(kIsWeb){
-     await Firebase.initializeApp(
-      options:const FirebaseOptions(
-        apiKey:"AIzaSyBjKIA41aULT9tKKV5l1Oxikzi2qD9LLQI", 
-        appId:"1:17950484937:web:3d16c98c9b1486644d6018",
-        messagingSenderId: "17950484937", 
-        projectId:"instagram-50a2a",
-      ),
-     );
-   }else{
-    await Firebase.initializeApp();
-   }
+void main(){
   runApp(
     const MyApp(),
   );
 }
 
-// this is root of your application
+// this is root of your application 
 
 class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+   const MyApp({super.key});
    @override
   Widget build(BuildContext context) {
-   return  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme:ThemeData.dark().copyWith(
-      scaffoldBackgroundColor:mobileBackgroundColor,
-    ),
-    home:MainScreen(),
-   );
+    return const MaterialApp(
+      home:Scaffold(
+        body:Center(child:Text('All india radio')),
+      ),
+    );
   }
 }
--- lib/utilis/colors.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-const mobileBackgroundColor = Color.fromRGBO(0, 0, 0,1);
